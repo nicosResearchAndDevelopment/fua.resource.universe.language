24879c1ba30671846044d735df233e98825cf8d7
Simon Petrac <pfoerdie@gmail.com>
2023-08-07 16:03:11 +0200

    split of universe into sub-packages

---
+++ src/load.js
@@ -0,0 +1,11 @@
+module.exports = {
+    '@context':        'fua.load.rdf',
+    'dct:identifier':  __filename,
+    'dct:format':      'application/fua.load+js',
+    'dct:title':       'load',
+    'dct:alternative': '@nrd/fua.resource.universe.language',
+    'dct:requires':    [{
+        'dct:identifier': '../data/languages.ttl',
+        'dct:format':     'text/turtle'
+    }]
+};
